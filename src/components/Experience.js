1e50dcdfa2438d830bdebe029c816223d77f7e5f
pavithra-_deepika <pavithradeepika301.com>
2022-12-07 15:03:42 +0530

    title and experience update

---
+++ src/components/Experience.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+export default function Experience() {
+    return (
+        <div id="heading">
+            <h3>Experience</h3>
+            <div id="experience">
+                <h3>Atom Healthcare - Front End Developer</h3>
+                <h5>june 2021 - july 2022</h5>
+                <ul>
+                    <li>Impemented wbsites and mobile applications and lnding page from concept through deployment</li>
+                    <li>Impemented wbsites and mobile applications and lnding page from concept through deployment</li>
+                </ul>    
+            </div>   
+        </div>
+    )
+}
---
+++ src/components/ProfileTitle.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function ProfileTitle(props) {
+    return (
+        <div id="title">
+            <h1>{props.name}</h1>
+            <h3>Front End Developer | React Developer</h3>
+        </div>
+    )
+}
+export default ProfileTitle
